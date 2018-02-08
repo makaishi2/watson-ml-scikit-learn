79d8c698dd3534b07f85ffe820c17e42f51c3ebd
赤石雅典 <makaishi@akaishiorinoair.dhcp.hakozaki.ibm.com>
2018-02-08 20:55:57 +0900

    init

---
+++ README.md
@@ -0,0 +1,113 @@
+# Watson Machine Learning サンプルアプリ
+
+## アプリケーションの説明
+このアプリケーションはIBM DSXで機械学習を行い、Watson Machine Learning上に保存、Webサービス化した機械学習モデルをWebサービスによりアクセスし、予測を行うサンプルアプリです。
+機械学習モデルの詳細は[Watson Machine Learning - IBM DSX 上のscikit-learn 機械学習モデルをデプロイする -
+](https://qiita.com/makaishi2/items/38371d272d752b6e7647)を参照して下さい。
+
+![](readme_images/wml-sample-1.png)  
+
+## 前提
+Watson DSX, Watson MLの導入と、Waton ML上のモデル保存、Webサービス化まで済んでいることを前提とします。  
+
+## 事前準備
+Watson MLのインスタンス名を``machine-learning-1``に変更して下さい。インスタンス名の変更は、サービスの管理画面から行うことが可能です。
+
+
+## 前提ソフトの導入
+次の前提ソフトが導入されていることが前提です。
+
+[gitコマンドラインツール][git]  
+[Cloud Foundryコマンドラインツール][cloud_foundry]  
+  
+注意: Cloud Foundaryのバージョンは最新として下さい。 
+
+## ソースのダウンロード
+Githubからアプリケーションのソースをダウンロードします。  
+カレントディレクトリのサブディレクトリにソースはダウンロードされるので、あらかじめ適当なサブディレクトリを作り、そこにcdしてから下記のコマンドを実行します。  
+ダウンロード後、できたサブディレクトリにcdします。
+ 
+
+```sh
+$ cd (適当なサブディレクトリ)
+$ git clone https://github.com/makaishi2/scikit-learn-sample.git
+$ cd scikit-learn-sample
+```
+
+## CFコマンドでログイン
+CFコマンドでbluemix環境にログインします。ログイン名、パスワードはBluemixアカウント登録で登録したものを利用します。  
+ログインに成功すると、次のような画面となります。  
+
+```
+$ cf api https://api.ng.bluemix.net
+$ cf login
+```
+
+![](readme_images/cf-login.png)  
+
+## アプリケーションのデプロイ
+
+次のコマンドを実行します。
+\<service_name\>はなんでもいいのですが、インターネット上のURLの一部となるので、ユニークな名前を指定します。  
+(例) scikit-learn-sample-aka1
+
+```
+$ cf push <service_name>
+```
+
+## 環境変数の設定
+
+WebサービスのエンドポイントURLに関しては、以下のコマンドで、設定する必要があります。  
+(注意) scoring_urlは、Waton Machine Learningの管理ダッシュボードより取得できますが、URLの最後に'/online'を付ける必要がある点に注意して下さい。
+
+```
+$ cf set-env <service_name> SCORING_URL <scoring_url>
+```
+
+## アプリケーションのURLと起動
+
+デプロイには数分かかります。デプロイが正常に完了したらアプリケーションを起動できます。  
+次のURLをブラウザから指定して下さい。
+
+```
+https://<service_name>.mybluemix.net/
+```
+
+## アプリケーションを修正する場合
+
+導入手順中、git cloneコマンドでダウンロードしたローカルリポジトリにアプリケーションのソースコードが入っています。アプリケーションを修正したい場合は、ローカルのソースを修正し、再度 "cf push \<service_name\>"コマンドを実行すると、Bluemix上のアプリケーションが更新されます。  
+
+## ローカルで起動する場合
+
+アプリケーションを修正する時は、ローカルでもテストできる方が便利です。そのための手順は以下の通りです。
+
+* Node.jsの導入  
+ローカルにNode.jsを導入する必要があります。
+[Node.jsダウンロード][node_js]からダウンロードして下さい。
+* 認証情報の確認  
+BluemixダッシュボードからWMLサービスの管理画面を開き、接続用の認証情報を調べてテキストエディタなどにコピーします。
+* .envファイルの設定
+次のコマンドで.env.exampleファイルの雛形から.envをコピーし、エディタで調べたusername, passwordを設定します。
+
+```sh
+$ cp .env.example .env
+```
+
+```sh
+WML_SERVICE_CREDENTIALS_URL=xxxxx
+WML_SERVICE_CREDENTIALS_USERNAME=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+WML_SERVICE_CREDENTIALS_PASSWORD=xxxxxxxxxxxx
+```
+
+* Node.jsアプリケーションの導入、実行  
+以下のコマンドでアプリケーションの導入、実行を行います。
+
+```sh
+$ npm install
+$ npm start
+```
+
+[cloud_foundry]: https://github.com/cloudfoundry/cli#downloads
+[git]: https://git-scm.com/downloads
+[sign_up]: https://bluemix.net/registration
+[node_js]: https://nodejs.org/ja/download/
---
+++ server.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import requests
+import os
+from flask import Flask, request, render_template
+from sklearn import datasets
+import numpy
+from PIL import Image
+import urllib3
+import requests
+import json
+from cfenv import AppEnv
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+# 認証情報の読み取り (.env または IBM Cloud上のバインド)
+env = AppEnv()
+pm20 = env.get_service(label='pm-20')
+if pm20 is None:
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+    wml_credentials = {
+        "url": os.environ.get("WML_URL"),
+        "access_key": os.environ.get("WML_ACCESS_KEY"),
+        "username": os.environ.get("WML_USERNAME"),
+        "password":  os.environ.get("WML_PASSWORD"),
+        "instance_id": os.environ.get("WML_INSTANCE_ID")
+    }
+else:
+    wml_credentials = pm20.credentials
+
+# スコアリングURLの読み取り (.env または環境変数)
+scoring_url = os.environ.get("SCORING_URL")
+
+# 手書き文字データの読み込み
+digits = datasets.load_digits()
+samples_count = len(digits.images)
+score_data = digits.data[int(0.9*samples_count): ]
+score_labels = digits.target[int(0.9*samples_count): ]
+score_images = digits.images[int(0.9*samples_count): ]
+
+app = Flask(__name__)
+
+@app.route('/')
+def top():
+    name = "Top"
+    return render_template('wml-sample.html', title='WML Test', name=name)
+
+# 「テスト対象選択」ボタンが押された時の処理
+@app.route('/select_image', methods=['GET'])
+def select_image():
+    print('/select_image')
+    image_id = int(request.args.get('image_id', ''))
+    image = score_images[image_id]
+    label = score_labels[image_id]
+    pilImg = Image.fromarray(numpy.uint8((16 - image) * 15))
+    pilImg.save('static/image.png')
+    return str(label)
+
+# 「予測」ボタンが押された時の処理
+@app.route('/predict', methods=['GET'])
+def predict():
+    print('/predict')
+    image_id = int(request.args.get('image_id', ''))
+    data = score_data[image_id]
+
+    # Basic認証用ヘッダの生成
+    auth = '{username}:{password}'.format(username=wml_credentials['username'], password=wml_credentials['password'])
+    header_basic_auth = urllib3.util.make_headers(basic_auth=auth)
+    url = '{}/v3/identity/token'.format(wml_credentials['url'])
+
+    # Tokenの取得
+    mltoken =  json.loads( requests.get(url, headers=header_basic_auth).text )['token']
+    print(mltoken)
+    header_token = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
+    # Webサービスの呼出し
+    payload_scoring = {"values": [list(data)]}
+    print(payload_scoring)
+    scoring_response = requests.post(scoring_url, json=payload_scoring, headers=header_token)
+    scoring = json.loads( scoring_response.text )
+
+    # 結果の取得
+    predict_list = [item[0] for item in scoring['values']]
+    predict_str = str(predict_list[0])
+    print(predict_str)
+    return predict_str
+
+@app.route('/favicon.ico')
+def favicon():
+   return ""
+
+port = os.getenv('VCAP_APP_PORT', '8000')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(port), debug=True)
